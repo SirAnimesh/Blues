0b74d5fc4bbb3ea7da4ea4556bf9120475b58b38
Animesh <hello@animesh.ltd>
2018-04-22 19:00:42 +0530

    Mass refactoring + documentation

--- client/assets/scripts/main.js
+++ client/assets/scripts/main.js
@@ -13,48 +13,78 @@ var chelseaScore = 0
 var juveScore = 0
 
 /**
- *  `onClick` event handler for the start button. Takes care of the following tasks:
- *      - disabling the Start button after kick-off
- *      - enabling the Reset button after kick-off
- *      - establishing a websockets connection with the server
- *      - managing the server-client communication
- *      - displaying final match result at full-time
+ *  Adds the given Chelsea event to the Chelsea column in the match table. Text is ouput as-is, the 
+ *  function expects pre-formatted string.
+ *  
+ *  @param {string} text    Event description 
+ * 
+ *  #TODO:
+ *  Refactor this and `AddJuveEvent` function into a generic function.
  */
-function Start() {
-    console.log("◊◊◊  Start() begins  ◊◊◊")
+function AddChelseaEvent(text) {
+    let label = document.createElement("Label")
+    label.innerHTML = text
+    label.setAttribute("class", "chelseaEvent")
 
-    var startButton = document.getElementById("start")
-    startButton.disabled = true
-    startButton.classList.add("disabled")
+    document.getElementById("chelseaEvents").appendChild(label)
+}
 
-    // Open a websockets connection and tell the server that the client is ready
-    var webSocket = new WebSocket("ws://localhost:8080")
-    webSocket.onopen = function (event) {
-        webSocket.send("Bring it on!"); 
+/**
+ *  Adds the given Juve event to the Juve column in the match table. Text is ouput as-is, the function
+ *  expects pre-formatted string.
+ *  
+ *  @param {string} text    Event description 
+ * 
+ *  #TODO:
+ *  Refactor this and `AddJuveEvent` function into a generic function.
+ */
+function AddJuveEvent(text) {
+    let label = document.createElement("Label")
+    label.innerHTML = text
+    label.setAttribute("class", "juveEvent")
+
+    document.getElementById("juveEvents").appendChild(label)
+}
+
+/**
+ *  Displays the final result of the match at full-time.
+ */
+function DeclareResult() {
+    let result = document.getElementById("result")
+
+    if (chelseaScore > juveScore) {
+        result.innerHTML = "Chelsea wins!"
     }
-
-    // The Websockets server sends a "Full-time" message to denote the end of a match data
-    // stream. The onmessage handler checks for this message and calls the DeclareResult()
-    // method to print the match result on the screen.
-    // Otherwise, match data events are inserted into the DOM.
-    //
-    // #TODO: Must think of a better way to handle this scenario
-    webSocket.onmessage = function (event) {
-        if(event.data == "Full-time") {
-            DeclareResult()
-
-            // Enable the Reset button
-            var reset = document.getElementById("stop")
-            reset.disabled = false
-            reset.classList.remove("disabled")
-            reset.style.cursor = "pointer"
-        }
-        else {
-            InsertMessage(JSON.parse(event.data))
-        }
+    else if (chelseaScore < juveScore) {
+        result.innerHTML = "Juventus wins!"
     }
+    else {
+        result.innerHTML = "It was a draw."
+    }
+}
 
-    console.log("◊◊◊  Start() ends  ◊◊◊")
+/**
+ *  Insert the time stamp for an event in the match table's minute column.
+ *  
+ *  @param {number} minute   The minute-mark for an event.
+ */
+function InsertTimeStamp(minute) {
+    let timestamp = document.createElement("Label")
+    timestamp.innerHTML = minute + "'"
+    timestamp.setAttribute("class", "minute")
+    document.getElementById("minuteEvents").appendChild(timestamp)
+}
+
+/**
+ *  Removes all children of the HTML element tagged with the given `elementID`.
+ * 
+ *  @param {string} elementID   `id` attribute of the HTML element to be cleaved 
+ */
+function RemoveChildren(elementID) {
+    let element = document.getElementById(elementID)
+    while(element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
 }
 
 /**
@@ -95,8 +125,81 @@ function Reset() {
     console.log("◊◊◊  Reset() ends  ◊◊◊")
 }
 
-function InsertMessage(message) {
-    AddTimeStamp(message.minute)
+/**
+ *  `onClick` event handler for the start button. Takes care of the following tasks:
+ *      - disabling the Start button after kick-off
+ *      - enabling the Reset button after kick-off
+ *      - establishing a websockets connection with the server
+ *      - managing the server-client communication
+ *      - displaying final match result at full-time
+ */
+function Start() {
+    console.log("◊◊◊  Start() begins  ◊◊◊")
+
+    var startButton = document.getElementById("start")
+    startButton.disabled = true
+    startButton.classList.add("disabled")
+
+    // Open a websockets connection and tell the server that the client is ready
+    var webSocket = new WebSocket("ws://localhost:8080")
+    webSocket.onopen = function (event) {
+        webSocket.send("Bring it on!"); 
+    }
+
+    // The Websockets server sends a "Full-time" message to denote the end of a match data
+    // stream. The onmessage handler checks for this message and calls the DeclareResult()
+    // method to print the match result on the screen.
+    // Otherwise, match data events are inserted into the DOM.
+    //
+    // #TODO: Must think of a better way to handle this scenario
+    webSocket.onmessage = function (event) {
+        if(event.data == "Full-time") {
+            DeclareResult()
+
+            // Enable the Reset button
+            var reset = document.getElementById("stop")
+            reset.disabled = false
+            reset.classList.remove("disabled")
+            reset.style.cursor = "pointer"
+        }
+        else {
+            UpdateMatchTable(JSON.parse(event.data))
+        }
+    }
+
+    // Closing the browser windows/tab without terminating the websocket connection will cause
+    // an error server-side. This handler ensures that the connection is neatly wrapped up before
+    // exiting.
+    window.onunload = function() {
+        webSocket.close()
+    }
+
+    console.log("◊◊◊  Start() ends  ◊◊◊")
+}
+
+/**
+ *  Insert the given team event message into the appropriate element in the match data DOM tree.
+ *  If a goal is reported, increments the chelseaScore/juveScore variables and calls `UpdateScore()`
+ *  to update the score panel at the top.
+ * 
+ *  @param {any} message  The event message received from the websockets server.
+ *  
+ *  Note:
+ *  `message` has the following properties: 
+ *  {
+ *      source: string,     // Origin of the message. Either of `Chelsea` or `Juventus` services
+ *      type: string,       // Event type: goal, foul, booking etc.
+ *      player: string,     // Player in action
+ *      minute: number      // Minute-mark for the event   
+ *  }
+ * 
+ *  #TODO:
+ *  The match data table is not actually a table right now. It's a giant mess of labels, divs and
+ *  <p> elements. Updating it to a table would make the website more performant for large datasets
+ *  and semantically correct too.
+ */
+function UpdateMatchTable(message) {
+    InsertTimeStamp(message.minute)
 
     if (message.source == "Chelsea") {
         if (message.type == "Goal" || message.type == "Goal(P)") { chelseaScore++ }
@@ -113,51 +216,11 @@ function InsertMessage(message) {
     UpdateScore()
 }
 
-function AddTimeStamp(minute) {
-    let timestamp = document.createElement("Label")
-    timestamp.innerHTML = minute + "'"
-    timestamp.setAttribute("class", "minute")
-    document.getElementById("minuteEvents").appendChild(timestamp)
-}
-
-function AddJuveEvent(text) {
-    let label = document.createElement("Label")
-    label.innerHTML = text
-    label.setAttribute("class", "juveEvent")
-
-    document.getElementById("juveEvents").appendChild(label)
-}
-
-function AddChelseaEvent(text) {
-    let label = document.createElement("Label")
-    label.innerHTML = text
-    label.setAttribute("class", "chelseaEvent")
-
-    document.getElementById("chelseaEvents").appendChild(label)
-}
-
-function RemoveChildren(divName) {
-    let div = document.getElementById(divName)
-    while(div.firstChild) {
-        div.removeChild(div.firstChild)
-    }
-}
-
+/**
+ *  Updates the score panel at the top of the match table. Called by the `UpdateMatchTable`
+ *  function when a goal event is reported.
+ */
 function UpdateScore() {
     var score = document.getElementById("score")
     score.innerHTML = chelseaScore + "   –   " + juveScore
-}
-
-function DeclareResult() {
-    let result = document.getElementById("result")
-
-    if (chelseaScore > juveScore) {
-        result.innerHTML = "Chelsea wins!"
-    }
-    else if (chelseaScore < juveScore) {
-        result.innerHTML = "Juventus wins!"
-    }
-    else {
-        result.innerHTML = "It was a draw."
-    }
 }
--- server/services.ts
+++ server/services.ts
@@ -8,25 +8,45 @@
 
 import * as RabbitMQ from "amqplib"
 
-class Message {
+/**
+ *  An Event instance describes notable incidents occuring throughout a game. E.g. goals, fouls,
+ *  bookings etc. 
+ */
+class Event {
+    /** Service from which the message originated */
     public source?: string
-    public type?: string
-    public player?: string
-    public minute?: number
-}
 
-const eventTypes = [
-    "Goal",
-    "Goal(P)",
-    "Foul",
-    "Booking(Y)",
-    "Booking(R)"
-]
+    /** Type of the event. Must be a member of the static `Types` array. */
+    public type?: string
+
+    /** Person of Interest */
+    public player?: string
+
+    /** Minute at which the incident occured */
+    public minute?: number
+
+    /** Supported event types */
+    public static Types = [
+        "Goal",
+        "Goal(P)",
+        "Foul",
+        "Booking(Y)",
+        "Booking(R)"
+    ]
+}
 
 // Binding and routing keys used by RabbitMQ
 const chelseaKey = "england.pl.chelsea" 
 const juveKey    = "italy.seriea.juve"
 
+/**
+ *  Simulates the `Chelsea` and `Juventus` services by randomly generating match events.
+ *  Publishes events to the exchange using the routing keys `england.pl.chelsea` and 
+ *  `italy.seriea.juve`.
+ *  
+ *  #TODO:
+ *  Swap this with actual inputs from the respective services.
+ */
 export async function Simulate() {
     // Connect with the local RabbitMQ server and create a channel
     let connection  = await RabbitMQ.connect("amqp://localhost")
@@ -36,7 +56,7 @@ export async function Simulate() {
     // Create a topic exchange
     channel.assertExchange(exchange, "topic", { durable: false })
 
-    // Bind queues to the exchange
+    // Bind queues to the topic exchange
     let chelsea = await channel.assertQueue("chelsea", { durable: false })
     channel.bindQueue(chelsea.queue, exchange, chelseaKey)
     
@@ -61,12 +81,13 @@ export async function Simulate() {
 
     let counter = 1
 
+    // Publish a random message to the queue every second
     let intervalID = setInterval(function() {
         let randomIndex = Math.floor(Math.random() * 5)
         if(counter % 2 == 0) {
-            let message     = new Message()
+            let message     = new Event()
             message.source  = "Chelsea"
-            message.type    = eventTypes[randomIndex]
+            message.type    = Event.Types[randomIndex]
             message.player  = chelseaPlayers[randomIndex]
             message.minute  = counter * 7
 
@@ -75,20 +96,20 @@ export async function Simulate() {
             channel.publish(exchange, chelseaKey, Buffer.from(JSON.stringify(message)))
         }
         else {
-            let message     = new Message()
+            let message     = new Event()
             message.source  = "Juventus"
-            message.type    = eventTypes[randomIndex]
+            message.type    = Event.Types[randomIndex]
             message.player  = juvePlayers[randomIndex]
             message.minute  = counter * 7
 
             // Publish a message to the MatchData exchange, from where it will be broadcasted to all
             // the queues subscribed to the topic juveKey (italy.seriea.juventus)
-            // Since we are persisting the message in our own databases, I've set the queue
             channel.publish(exchange, juveKey, Buffer.from(JSON.stringify(message)))
         }
         counter++
     }, 1000)
 
+    // Clear out the message publish loop after 14 seconds
     setTimeout(function() {
         clearInterval(intervalID)
     }, 14000)
